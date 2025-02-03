00cc556e2fae1318026f5a7a83897a2b2df114db
JichiUltimati <aly-bora.ulusoy@polytechnique.edu>
2025-02-03 15:16:15 +0100

    Quartz sync: Feb 3, 2025, 3:16 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'jichiultimati/recettes',
+        // from data-repo-id
+        repoId: 'R_kgDONyS5jg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONyS5js4CmnW6',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
