68039b1a61e88fe22ecbfeab5eaa6e8d62e4cd64
JichiUltimati <aly-bora.ulusoy@polytechnique.edu>
2025-02-06 20:17:41 +0100

    Quartz sync: Feb 6, 2025, 8:17 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,8 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/JichiUltimati/Recettes",
+      "Quartz GitHub": "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
